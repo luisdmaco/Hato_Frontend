cebd8d42be848a370b36942fc29962abfe44d910
Luis D. Maco <luisdmaco@outlook.com>
2023-05-04 18:45:00 -0500

    netify json-server setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,9 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "trabajoparcial";
+  constructor(private http: HttpClient) {
+    this.http.get("http://localhost:3000/Marriotelo").subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
